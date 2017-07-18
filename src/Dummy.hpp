eb9b9ca811a9b0f4ca5a9cd0a0b18ad6ade311c4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-07-17 21:37:11 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace plain_cxx11_package
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace plain_cxx11_package
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <plain_cxx11_package/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    plain_cxx11_package::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
